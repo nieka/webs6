5572b82bde3d1c07118ca663b1867392f98fe34f
raymondphua <chrphua@student.avans.nl>
2016-04-19 15:49:07 +0200

    add game

--- app/Game/Controllers/GameController.js
+++ app/Game/Controllers/GameController.js
@@ -1,15 +1,23 @@
 /**
  * Created by niek on 18-4-2016.
  */
+
+var Game = require('../Models/Game');
+
 // GameController.js
 module.exports = function($scope, GameFactory) {
 
     this.games = GameFactory.games;
 
-    $scope.joinGame = function(id){
+    this.joinGame = function(id){
         var game = GameFactory.getGame(id);
         game.addPlayer(123345); // fake id
         console.log("gebruiker toegevoegd");
         console.log(game);
+    };
+
+    this.newGame = function() {
+        this.games.push(new Game(9, 'Game 9', "Not Active", [], "6-4-2016"));
+        console.log('new game added');
     }
 };
