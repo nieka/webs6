ae83648546311805c3eb303db76826e5f9315edc
raymondphua <chrphua@student.avans.nl>
2016-05-08 22:16:45 +0200

    Logica added: check if tile is clickable

--- app/Game/Models/Board.js
+++ app/Game/Models/Board.js
@@ -38,6 +38,61 @@ function getTile(id){
     }
 }
 
+function checkAvailable(tile) {
+
+    var left = hasTileLeft(tile);
+    var right = hasTileRight(tile);
+    var top = hasTileTop(tile);
+
+    //if the tile has no tiles left or right and not on top return true else false
+    return (!left || !right) && !top;
+}
+
+//checks if the chosen tile has a tile to the left
+function hasTileLeft(tile) {
+    var self = this;
+    var hasLeft = false;
+
+    angular.forEach(self.boardTiles, function(value, key) {
+
+        if ((tile.xPos - 2) === value.xPos && tile.yPos === value.yPos && tile.zPos === value.zPos) {
+            hasLeft = true;
+        }
+    });
+
+    return hasLeft;
+}
+
+//checks if the chosen tile has a tile to the right
+function hasTileRight(tile) {
+    var self = this;
+    var hasRight = false;
+
+    angular.forEach(self.boardTiles, function(value, key) {
+
+        if ((tile.xPos + 2) === value.xPos && tile.yPos === value.yPos && tile.zPos === value.zPos) {
+            hasRight = true;
+        }
+    });
+
+    return hasRight;
+}
+
+//checks if the chosen tile has a tile on top
+function hasTileTop(tile) {
+    var self = this;
+    var hasTop = false;
+
+    angular.forEach(self.boardTiles, function(value, key) {
+
+        if(tile.xPos === value.xPos && tile.yPos === value.yPos && (tile.zPos + 1) === value.zPos) {
+            hasTop = true;
+        }
+    });
+
+    return hasTop;
+}
+
 //get game tiles
 Board.prototype.getBoardTiles = function() {
     return this.boardTiles;
